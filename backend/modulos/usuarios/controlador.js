0e6974f5e44c772a36cce927b14047bf39d8bf52
Yeinner19 <yeinnerramirez417@gmail.com>
2025-07-07 11:53:26 -0500

    Actualización de crear.js

---
+++ backend/modulos/usuarios/controlador.js
@@ -0,0 +1,4 @@
+const consultas = require("./consultas");
+const { exito, error} = require("../../red/respuesta")
+
+
--- frontend/js/crear.js
+++ frontend/js/crear.js
@@ -1,51 +1,58 @@
-document.addEventListener("DOMContentLoaded",()=>{
-    
-    const formulario = document.getElementById("form-crear-producto"); //Se obtiene el formulario desde el html por su ID
-    const mensaje = document.getElementById("mensaje"); //Se obtiene un mensaje que mostrará "éxito" o "error" desde el html por su ID
+document.addEventListener("DOMContentLoaded", ()=>{
+    const formulario = document.getElementsById("form-crear-producto");
+    const mensaje = document.getElementById("mensaje");
     const vistaPrevia = document.getElementById("vista-previa");
 
-    
-    formulario.addEventListener("submit",(e)=>{
-        e.preventDefault(); //Esto sirve para que la página no se recargue cuando se oprima el botón de submit
+    addEventListener("submit",(e)=>{
+        e.preventDefault(); //Evitar que se recargue la página
 
         const producto = {
-            id: Date.now(),
             nombre: document.getElementById("nombre").value.trim(),
             descripcion: document.getElementById("descripcion").value.trim(),
             precio: parseFloat(document.getElementById("precio").value),
             imagen: document.getElementById("imagen").value.trim()
-        };  
-        
-        if(!producto.nombre || !producto.descripcion || isNaN(producto.precio || !producto.imagen)){
-            mensaje.innerHTML = `<div class="alert alert-danger">Por favor completa todos los campos correctamente </div>`;
+        };
+
+        if( !producto.nombre || !producto.descripcion || isNaN(producto.precio)  || !producto.imagen){
+            mensaje.innerHTML = `<div class="alert alert-danger">❌ Por favor completa todos los campos correctamente</div>`;
             return;
         }
 
-
-        const productosGuardados = JSON.parse(localStorage.getItem("productos")) || [];
-
-        productosGuardados.push(producto);
-
-        localStorage.setItem("productos", JSON.stringify(productosGuardados));
+        //Enviar al backend (API REST con node.js + mysql)
         
-        mensaje.innerHTML = `<div class="alert alert-success"> Producto creado correctamente-</div>`;
+        fetch("http:/localhost:3000/api/producto/crear",{
+            method: "POST",
+            headers: {
+                "Content-Type":"application/json    "
+            },
+            body:JSON.stringify(producto) //convierte el objeto JS a JSON
+        })
 
+        .then(res => res.json())
+        .then(data =>{
+            if (data.success) {
+                mensaje.innerHTML = `<div class="alert alert-success">✅ Producto creado correctamente.</div>`;
+                formulario.reset();
 
-        vistaPrevia.innerHTML += `
-        <div class="col">
-            <div class="card h-100 shadow-sm">
-                <img src="${producto.imagen}" class="card-img-top" alt="Nombre del Producto 4">
-                <div class="card-body d-flex flex-column">
-                    <h5 class="card-title">${producto.nombre}</h5>
-                    <p class="card-text text-muted">${producto.descripcion}</p>
-                    <p class="card-text fs-4 fw-bold mt-auto">$${producto.precio.toLocaleString("es-CO")}</p>
-                    <a href="#" class="btn btn-primary mt-2">Añadir al Carrito</a>
-                </div>
-            </div>
-        </div>
-        `
-        
-        formulario.reset();
-
+                vistaPrevia += `<div class="col">
+                        <div class="card h-100 shadow-sm">
+                            <img src="${producto.imagen}" class="card-img-top" alt="Producto">
+                            <div class="card-body d-flex flex-column">
+                                <h5 class="card-title">${producto.nombre}</h5>
+                                <p class="card-text text-muted">${producto.descripcion}</p>
+                                <p class="card-text fs-4 fw-bold mt-auto">$${producto.precio.toLocaleString("es-CO")}</p>
+                                <a href="#" class="btn btn-primary mt-2">Añadir al Carrito</a>
+                            </div>
+                        </div>
+                    </div>`
+            }else{
+                mensaje.innerHTML = `<div class="alert alert-success">⚠️${data.error}</div>`
+            }
+        })
+        //si la petición falló completamente (ejm: servidor caído)
+        .catch(err => {
+            console.log("❌ Error al cargar productos", err);
+            mensaje.innerHTML = `<div class="alert alert-danger">Error al conectar con el servidor</div>`;
+        });
     });
 });
